a4a8404784bbcd6abcd15c7316642be9b376f55a
Varsha Aggarwal <aggarwalvarsha19@gmail.com>
2020-09-14 17:32:00 +0530

    commit

--- src/components/CountdownForm.jsx
+++ src/components/CountdownForm.jsx
@@ -5,8 +5,8 @@ class CountdownForm extends React.Component {
      onSubmit(e) {
        //This will give you string for seconds. Do not remove refs
        e.preventDefault();
-       var secondsStr = this.refs.seconds.value;
-       console.log(secondsStr);
+       var secondsStr = +this.refs.seconds.value;
+        if(secondsStr>0)
         this.props.onSetCountdownTime(secondsStr);
         this.refs.seconds.value = "";
     }
@@ -16,7 +16,7 @@ class CountdownForm extends React.Component {
         return (
             <div>
                 <form ref="form" onSubmit={this.onSubmit.bind(this)} className="countdown-form">
-                    <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+                    <input type="text" ref="seconds" placeholder="Enter time in seconds" min = "0" />
                     <input type="submit" className="button success expanded" value="Start Countdown"/>
                 </form>
              
